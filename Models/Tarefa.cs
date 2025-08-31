081d6bebbf39cc280ebc9f8b20d56c5909fd0e5d
ross-lh <lhbenross@gmail.com>
2025-08-31 05:48:36 -0300

    updated project to NET 8 with all its nuget packages

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TrilhaApiDesafio.Models
 {
     public class Tarefa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
